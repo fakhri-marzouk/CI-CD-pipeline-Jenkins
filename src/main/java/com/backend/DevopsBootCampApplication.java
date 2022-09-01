67caffc0d809ad7391dfb260ebb27db45c0ceeaf
fakhri <fakhrimarzouki199@gmail.com>
2022-09-01 21:52:09 +0100

    database connection

---
+++ src/main/java/com/backend/DevopsBootCampApplication.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsBootCampApplication.class, args);
+	}
+
+}
